d870661b140e5cdc6cc321118dee8f8d522423ce
Sane Aalam <saneraza78692@gmail.com>
2023-12-19 19:42:51 +0530

    Update Bubble Sort.ADD

--- Level - 03 Sorting Algorithm/01-bubble-sort.cpp
+++ Level - 03 Sorting Algorithm/01-bubble-sort.cpp
@@ -10,7 +10,12 @@ class Solution
         for(int i = 0; i<n; i++){
             for(int j = i+1; j<n; j++){
                 if(arr[i] > arr[j]){
-                    swap(arr[i],arr[j]);
+                    // swap(arr[i],arr[j]);
+                     // compare two adjacent elements
+                     // change > to < to sort in descending order
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
                 }
             }
         }
